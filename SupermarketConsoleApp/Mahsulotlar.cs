47a4233d1e75aee72a990eabe27056eb67b878ba
Markroft <mailmarkroft@gmail.com>
2023-02-15 15:19:23 +0500

    Add project files.

---
+++ SupermarketConsoleApp/Mahsulotlar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketConsoleApp
+{
+    public class Mahsulotlar
+    {
+        public int id { get; set; }
+
+        public string nomi { get; set; }
+
+        public int narxi { get; set; }
+
+        public string yaroqlilikMuddati { get; set;  }
+    }
+}
